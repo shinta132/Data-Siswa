cf5fdc17cefdc7c8a1bcc75b2f1e5471ab426e8f
shinta132 <sintaalfat6@gmail.com>
2022-04-22 22:05:39 +0700

    Layout
    Tugas UAS (layout)

---
+++ app/src/main/java/com/shinta/datasiswa/Update.kt
@@ -0,0 +1,11 @@
+package com.shinta.datasiswa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Update : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
